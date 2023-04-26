ceb26325bb52dc8a40b7a1bacc443547b1e05ec5
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-26 16:19:13 -0400

    Added events and ability to delete notes

--- contracts/TinyNotesToken.sol
+++ contracts/TinyNotesToken.sol
@@ -16,15 +16,20 @@ contract TinyNotesToken is ERC20 {
 
     mapping(uint256 => Note) private idToNote;
 
+    event NoteCreated(uint256 indexed noteId, address creator, string title, string content);
+    event NoteUpdated(uint256 indexed noteId, string newTitle, string newContent);
+    event NoteDeleted(uint256 indexed noteId);
+
     constructor(string memory _genesisTitle, string memory _genesisContent) ERC20("Tiny Notes Token", "TNT") {
         _mint(msg.sender, 47000000 * 10 ** decimals());
         idToNote[0] = Note(msg.sender, _genesisTitle, _genesisContent);
     }
 
     function createNote(string memory _title, string memory _content) public {
-        require(balanceOf(address(msg.sender)) > 0, "Must hold TNT to create a Note.");
+        require(balanceOf(msg.sender) > 0, "Must hold TNT to create a Note.");
         _noteIds.increment();
         idToNote[_noteIds.current()] = Note(msg.sender, _title, _content);
+        emit NoteCreated(_noteIds.current(), msg.sender, _title, _content);
     }
 
     function readNote(uint256 _noteId) public view returns (string memory title, string memory content) {
@@ -36,5 +41,12 @@ contract TinyNotesToken is ERC20 {
         require(idToNote[_noteId].creator == msg.sender, "Only the creator may update a Note.");
         idToNote[_noteId].title = _newTitle;
         idToNote[_noteId].content = _newContent;
+        emit NoteUpdated(_noteId, _newTitle, _newContent);
+    }
+
+    function deleteNote(uint256 _noteId) public {
+        require(idToNote[_noteId].creator == msg.sender, "Only the creator may delete a Note.");
+        delete idToNote[_noteId];
+        emit NoteDeleted(_noteId);
     }
 }
