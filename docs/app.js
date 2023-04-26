a76b89001d06199de9392d2cba56f73887647707
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-26 19:18:48 -0400

    Updated display notes for new contract

--- docs/app.js
+++ docs/app.js
@@ -8,37 +8,23 @@ async function getContractABI() {
 async function displayNotes(contract) {
     const notesContainer = document.getElementById("notes");
     notesContainer.innerHTML = ""; // Clear the container before adding new notes
-  
-    let noteExists = true;
-    let noteId = 0;
-  
-    while (noteExists) {
-      try {
-        // Assuming your contract has a function getNote() that takes a note ID and returns the note data
+
+    const noteIds = await contract.methods.noteIds().call(); // Use the public noteIds getter
+
+    for (const noteId of noteIds) {
         const noteData = await contract.methods.readNote(noteId).call();
         const noteElement = document.createElement("div");
         noteElement.className = "note";
         noteElement.innerHTML = `
-          <h3>${noteData.title}</h3>
-          <p>${noteData.content}</p>
-          <button onclick="updateNote(${noteId})">Update</button>
-          <button onclick="deleteNote(${noteId})">Delete</button>
+            <h3>${noteData.title}</h3>
+            <p>${noteData.content}</p>
+            <button onclick="updateNote(${noteId})">Update</button>
+            <button onclick="deleteNote(${noteId})">Delete</button>
         `;
-
         notesContainer.appendChild(noteElement);
-  
-        // If you want to show only notes created by the current user
-        // if (noteData.creator === account) {
-        //   notesContainer.appendChild(noteElement);
-        // }
-  
-        noteId++; // Increment noteId for the next iteration
-      } catch (error) {
-        // If the note with the current ID does not exist, exit the loop
-        noteExists = false;
-      }
     }
 }
+
 
 window.addEventListener('load', async () => {
     if (window.ethereum) {
