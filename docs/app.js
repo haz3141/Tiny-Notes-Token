de35b28d8652895f409de84a4fd2ced8dfb9bd53
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-28 15:47:59 -0400

    Updated to hide deleted notes

--- docs/app.js
+++ docs/app.js
@@ -7,7 +7,6 @@ async function initWeb3() {
   if (typeof window.ethereum !== "undefined") {
     web3 = new Web3(window.ethereum);
     accounts = await web3.eth.getAccounts();
-    balance = displayBalance();
 
     // Load ABIs
     const tokenABI = await loadABI("TinyNotesTokenABI.json");
@@ -30,7 +29,8 @@ async function initWeb3() {
       .addEventListener("click", createNote);
 
     // Toggle create note button
-    toggleCreateNoteButton(balance);
+    balance = await displayBalance();
+    await toggleCreateNoteButton(balance);
 
     // Load existing notes
     loadNotes();
@@ -43,6 +43,9 @@ async function requestTokens() {
   try {
     await faucetContract.methods.requestTokens().send({ from: accounts[0] });
     alert("Tokens successfully requested!");
+    // Toggle create note button
+    balance = displayBalance();
+    toggleCreateNoteButton(balance);
   } catch (error) {
     console.error(error);
     alert("Error requesting tokens. Check the console for more information.");
@@ -65,6 +68,16 @@ async function createNote() {
   }
 }
 
+async function deleteNote(noteId) {
+  try {
+    await tokenContract.methods.deleteNote(noteId).send({ from: accounts[0] });
+    alert("Note deleted successfully");
+    location.reload();
+  } catch (err) {
+    alert("Error deleting note: " + err.message);
+  }
+}
+
 async function loadNotes() {
   const notesDiv = document.getElementById("notes");
   notesDiv.innerHTML = "";
@@ -73,14 +86,37 @@ async function loadNotes() {
 
   for (let noteId = 0; noteId <= totalNotes; noteId++) {
     const noteData = await tokenContract.methods.readNote(noteId).call();
-    const noteElement = document.createElement("div");
-    noteElement.className = "note";
-    noteElement.innerHTML = `
+
+    if (noteData.title !== "") {
+      const noteElement = document.createElement("div");
+      noteElement.className = "note";
+      noteElement.innerHTML = `
             <h2 class="note-title">${noteData.title}</h2>
             <p class="note-content">${noteData.content}</p>
             <p class="note-creator">Created by: ${noteData.creator}</p>
-        `;
-    notesDiv.appendChild(noteElement);
+    `;
+
+      if (noteData.creator.toLowerCase() === accounts[0].toLowerCase()) {
+        const updateButton = document.createElement("button");
+        updateButton.className = "update-button";
+        updateButton.textContent = "Update";
+        updateButton.addEventListener("click", () => {
+          // Handle updating the note here
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete-button";
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", async () => {
+          await deleteNote(noteId);
+        });
+
+        noteElement.appendChild(updateButton);
+        noteElement.appendChild(deleteButton);
+      }
+
+      notesDiv.appendChild(noteElement);
+    }
   }
 }
 
